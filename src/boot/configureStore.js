49f1fb5490df11c69ffef053c521793662575612
Przemek <sniffoza@gmail.com>
2018-08-02 22:09:48 +0200

    Linting - use single quote

--- src/boot/configureStore.js
+++ src/boot/configureStore.js
@@ -1,16 +1,16 @@
 // @flow
-// import { AsyncStorage } from "react-native";
-import devTools from "remote-redux-devtools";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { persistStore } from "redux-persist";
-import reducer from "../reducers";
+// import { AsyncStorage } from 'react-native';
+import devTools from 'remote-redux-devtools';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist';
+import reducer from '../reducers';
 
 export default function configureStore(onCompletion: () => void): any {
   const enhancer = compose(
     applyMiddleware(thunk),
     devTools({
-      name: "nativestarterkit",
+      name: 'nativestarterkit',
       realtime: true
     })
   );
