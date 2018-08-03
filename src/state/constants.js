702de0f50fc3a75483878fa3c4f0c5d273a08eef
Przemek <sniffoza@gmail.com>
2018-08-03 22:47:08 +0200

    Use AsyncStorage multiget to retrieve tokens and set headers

---
+++ src/state/constants.js
@@ -0,0 +1,8 @@
+const GET = 'GET';
+const POST = 'POST';
+const PUT = 'PUT';
+const DELETE = 'DELETE';
+
+export {
+  GET, POST, PUT, DELETE,
+}
--- src/stories/screens/BlankPage/index.js
+++ src/stories/screens/BlankPage/index.js
@@ -2,11 +2,21 @@ import * as React from "react";
 import { Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body } from "native-base";
 
 import styles from "./styles";
+import call from "../../../services/api";
+import { GET } from "../../../state/constants";
 export interface Props {
 	navigation: any;
 }
 export interface State {}
 class BlankPage extends React.Component<Props, State> {
+	componentDidMount() {
+		call({ url: '/lists', method: GET }).then((response) => {
+			console.log(response);
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
+
 	render() {
 		const param = this.props.navigation.state.params;
 		return (
@@ -27,6 +37,7 @@ class BlankPage extends React.Component<Props, State> {
 
 				<Content padder>
 					<Text>{param !== undefined ? param.name.item : "Create Something Awesome . . ."}</Text>
+					<Text>You are logged in!!!!</Text>
 				</Content>
 			</Container>
 		);
